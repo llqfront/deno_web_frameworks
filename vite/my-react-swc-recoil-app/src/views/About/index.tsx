87843c826da671f3088f6d2d96a25c4b0599fb32
llqfront <llqfront@aliyun.com>
2023-03-03 20:12:13 +0800

    fetch add

--- vite/my-react-swc-recoil-app/src/views/About/index.tsx
+++ vite/my-react-swc-recoil-app/src/views/About/index.tsx
@@ -1,12 +1,21 @@
 import React, { memo } from 'react';
-import { atom, selector, useRecoilState, useRecoilValue } from "recoil";
+import { atom, selector, selectorFamily, useRecoilState, useRecoilValue } from "recoil";
 import { charCountState, textState } from "@/stores/index.ts";
+const userSelector = selectorFamily({
+  key: 'user selector family',
+  get: (userId) => async () => {
+    const response = await fetch(`/api/${userId}`);
+    return await response.json()
+  }
+});
 
 interface Props {
 }
 const View: React.FC = (props: Props) => {
     const [text, setText] = useRecoilState(textState);
     const count = useRecoilValue(charCountState);
+    const user = useRecoilValue(userSelector('advertise'));
+    console.log(user.data)
     return (
       <React.Fragment>
         Aboout<br/>
