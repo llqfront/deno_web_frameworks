f64642acff8707c989b717ccdae8584e0a2633dd
llqfront <llqfront@aliyun.com>
2023-03-03 17:19:29 +0800

    my-react-swc-recoil-app  dist is ok  test.youdeal.top

---
+++ vite/my-react-swc-recoil-app/src/views/About/index.tsx
@@ -0,0 +1,12 @@
+import React, { memo } from 'react';
+interface Props {
+}
+const View: React.FC = (props: Props) => {
+
+    return (
+      <React.Fragment>
+        Aboout
+      </React.Fragment>
+    )
+}
+export default memo(View);
---
+++ vite/my-react-swc-recoil-app/src/views/Demo/index.tsx
@@ -0,0 +1,12 @@
+import React, { memo } from 'react';
+interface Props {
+}
+const View: React.FC = (props: Props) => {
+
+    return (
+      <React.Fragment>
+        demo
+      </React.Fragment>
+    )
+}
+export default memo(View);
